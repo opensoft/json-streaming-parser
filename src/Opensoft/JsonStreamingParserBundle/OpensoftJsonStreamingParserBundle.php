89baeef72818a646898b34a741e282b4ab3f0380
Vladimir Prudilin <bstrxx@gmail.com>
2016-02-17 17:02:32 +0300

    DI extension added

--- src/Opensoft/JsonStreamingParserBundle/OpensoftJsonStreamingParserBundle.php
+++ src/Opensoft/JsonStreamingParserBundle/OpensoftJsonStreamingParserBundle.php
@@ -1,14 +1,5 @@
 <?php
-/**
- * This file is part of ONP.
- *
- * Copyright (c) 2013 Opensoft (http://opensoftdev.com)
- *
- * The unauthorized use of this code outside the boundaries of
- * Opensoft is prohibited.
- */
-
-namespace Opensoft\Onp\Bundle\ApiBundle;
+namespace Opensoft\JsonStreamingParserBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
